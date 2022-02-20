bb4d2b7725a205b160f23cade4f368e06cd7fa93
blueback <blueback28@gmail.com>
2022-02-20 10:52:20 +0900

    整理

---
+++ BlueBackScene/Assets/UPM/Runtime/BlueBack/Scene/Execute_Base.cs
@@ -0,0 +1,24 @@
+
+
+/**
+ * Copyright (c) blueback
+ * Released under the MIT License
+ * @brief ÉVÅ[ÉìÅB
+*/
+
+
+/** BlueBack.Scene
+*/
+namespace BlueBack.Scene
+{
+	/** Execute_Base
+	*/
+	public interface Execute_Base
+	{
+		/** [BlueBack.Scene.Execute_Base]Boot
+		*/
+		System.Collections.IEnumerator Boot();
+	}
+}
+
+
